f9118b4f3abe6f143e891c366992b9b2098e4c03
Éloi Strée <eloistree@gmail.com>
2023-07-14 23:13:10 +0200

    Save version before rework

---
+++ VisualCode/RestreamChatHacking/RestreamChatHacking/SeleniumAccessToRestreamChat.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System.Xml.XPath;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium.IE;
+using System.Drawing;
+
+namespace RestreamChatHacking
+{
+    public class SeleniumAccessToRestreamChat
+    {
+
+
+        private IWebDriver m_driver; 
+        public enum NavigatorType { Firefox, Chrome, InternetExplorer }
+
+
+
+        public void SetupSeleniumDriver(bool useDebug, NavigatorType navigatorType)
+        {
+            if (m_driver == null) {
+
+                if (navigatorType == NavigatorType.Firefox)
+                    m_driver = new FirefoxDriver();
+                else
+                if (navigatorType == NavigatorType.InternetExplorer)
+                    m_driver = new InternetExplorerDriver();
+                else {
+                    string pathDriver = Environment.CurrentDirectory+ "\\GoogleDriver";
+                    Console.WriteLine("Driver:" +pathDriver);
+                    m_driver = new ChromeDriver(pathDriver);
+                 }
+
+
+
+                Thread.Sleep(2000);
+                if (ChatHackerConfiguration.Instance.IsUserRequestToHideInterface())
+                {
+                    m_driver.Manage().Window.Size = new System.Drawing.Size(0, 0);
+                    m_driver.Manage().Window.Position = new System.Drawing.Point(-2000, 0);
+                }
+                else
+                {
+                    m_driver.Manage().Window.Size = new System.Drawing.Size(300, 200);
+                }
+            }
+
+
+        }
+        public void OpenPage(string url) {
+            if (m_driver == null)
+                SetupSeleniumDriver(false, NavigatorType.Chrome);
+            m_driver.Navigate().GoToUrl(url);
+        }
+
+
+        public bool IsNavigatorOpen()
+        {
+            bool isOpen = true;
+            try
+            {
+                m_driver.WindowHandles.Count();
+            }
+            catch (InvalidOperationException e)
+            {
+                isOpen = false;
+            }
+
+            return isOpen;
+        }
+
+
+        public bool IsDriverInstanciated()
+        {
+            return m_driver != null;
+        }
+       
+
+        public string GetRestreamHtmlPageInformation()
+        {
+            if (m_driver == null)
+                return "";
+            return m_driver.PageSource;
+        }
+        public void TeardownRunningDriver()
+        {
+            if (m_driver != null)
+                m_driver.Quit();
+            //try
+            //{
+            //    m_driver.Quit();
+            //}
+            //catch (Exception)
+            //{
+
+            //    // Ignore errors if unable to close the browser
+            //}
+        }
+
+        /// <summary>
+        /// I keep this code just in case I need it later. Was used in the V0 of the project.
+        /// </summary>
+        public class Archived {
+
+            public void StartToListenAtRestreamEmbedUrl(string embedUrl)
+            {
+                //m_driver.Navigate().GoToUrl(url);
+                while (true)
+                {
+                    //ARCHIVED WHILE LISTENING
+                    //m_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                    //System.Threading.Thread.Sleep(m_frameTiming);
+                }
+
+            }
+            private bool IsAlertPresent()
+            {
+                try
+                {
+                   // m_driver.SwitchTo().Alert();
+                    return true;
+                }
+                catch (NoAlertPresentException)
+                {
+                    return false;
+                }
+            }
+
+            public bool m_acceptNextAlert;
+            private string CloseAlertAndGetItsText()
+            {
+                try
+                {
+                    IAlert alert = null;// m_driver.SwitchTo().Alert();
+                    string alertText = alert.Text;
+                    if (m_acceptNextAlert)
+                    {
+                        alert.Accept();
+                    }
+                    else
+                    {
+                        alert.Dismiss();
+                    }
+                    return alertText;
+                }
+                finally
+                {
+                    m_acceptNextAlert = true;
+                }
+            }
+
+        }
+
+        
+    }
+
+
+}
---
+++ VisualCode/RestreamChatHacking/RestreamChatHacking/User32Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestreamChatHacking
+{
+    class User32Utility
+    {
+        public static void HideTheInterfaceWithUser32DLL()
+        {
+            var openWindows = Process.GetProcesses().Where(process => String.IsNullOrEmpty(process.MainWindowTitle) == false);
+            SetWindowPos(Process.GetCurrentProcess().MainWindowHandle, IntPtr.Zero, 0, -2000, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            ShowWindowAsync(Process.GetCurrentProcess().MainWindowHandle, SW_SHOWMINIMIZED);
+        }
+
+
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_SHOWMINIMIZED = 2;
+        private const int SW_SHOWMAXIMIZED = 3;
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOZORDER = 0x0004;
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+
+    }
+}
